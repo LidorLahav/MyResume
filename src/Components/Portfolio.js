af3c7fb82553bf0b992eb459fca2d6f559553f18
Lidor Lahav <lidorlahav@gmail.com>
2021-08-22 13:38:06 +0300

    change link to header

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -2,17 +2,16 @@
 import React from 'react'
 
 const Portfolio = (props) => {
-    if(props.data){
-        var projects = props.data.projects.map((project)=>{
+    if (props.data) {
+        var projects = props.data.projects.map((project) => {
             return <div className="columns feature-item" key={project.title}>
-                <h3><span>{project.title}</span></h3>
+                <h3><a href={project.url}><span>{project.title}</span></a></h3>
                 <div>{project.description}</div>
-                <a href={project.url}>link</a>
             </div>
         })
     }
 
-    return(
+    return (
         <section id="portfolio">
             <h1>Check out my projects</h1>
             <ul className="bgrid-quarters s-bgrid-thirds cf">
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -15,18 +15,14 @@ class Resume extends Component {
         if (!this.props.data) return null;
 
         const skillmessage = this.props.data.skillmessage;
-        const education = this.props.data.education.map(function (education) {
-            return (
-                <div key={education.school}>
-                    <h3>{education.school}</h3>
-                    <p className="info">
-                        {education.degree} <span>&bull;</span>
-                        <em className="date">{education.graduated}</em>
-                    </p>
-                    <p>{education.description}</p>
-                </div>
-            );
-        });
+        var education = this.props.data.education.map((education) => {
+            var description = education.description.map((line) => {
+                return <div key={line}>{line}</div>
+            })
+            return <div key={education.school}><h3>{education.school}</h3>
+                <p className="info">{education.degree}<span>&bull;</span><em className="date">{education.graduated}</em></p>
+                <div className="description">{description}</div></div>
+        })
 
         const work = this.props.data.work.map(function (work) {
             return (
